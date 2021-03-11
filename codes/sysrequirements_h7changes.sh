6579538b7041d642160b39b366bbece1d6ab2814
Saptarshi Majumdar <saptahash@users.noreply.github.com>
2021-03-11 18:48:45 +0000

    adding data.table to the installation system dependencies

--- codes/sysrequirements_h7changes.sh
+++ codes/sysrequirements_h7changes.sh
@@ -3,5 +3,5 @@ sudo apt-get install libcurl4-openssl-dev
 sudo apt install -y libudunits2-0 libudunits2-dev
 sudo apt install libgdal-dev
 Rscript -e 'install.packages("remotes")'
-Rscript -e 'remotes::install_cran(c("lubridate", "tidyverse", "readr"))'
+Rscript -e 'remotes::install_cran(c("lubridate", "tidyverse", "readr", "data.table"))'
 Rscript -e 'remotes::install_github("hadley/dplyr", build_vignettes = FALSE)'
